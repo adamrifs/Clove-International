58a0f7f0a2b411571ec5e8e4e9efdf8398b6617a
adamrifs <rifsadam81@gmail.com>
2024-11-14 20:12:13 +0530

    ready to deploy

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,5 +1,5 @@
 const users = require('../Models/userSchema')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const dotenv = require('dotenv')
 dotenv.config()
