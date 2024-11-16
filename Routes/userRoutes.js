b24df3e12ed39f42c1cf2524bf18de92f119693b
adamrifs <rifsadam81@gmail.com>
2024-11-16 12:11:31 +0530

    updates

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../Controllers/userController')
+const userMiddleware = require('../Middleware/userMiddleware')
+
 
 router.post('/register', userController.register)
-router.post('/login',userController.login)
-router.get('/getUser',userController.getUser)
-router.put('/edituser/:id',userController.edituser)
-router.get('/getuserwithinvestment/:id',userController.getuserwithinvestment)
-router.get('/getuserkyc/:id',userController.getuserkyc)
-router.put('/adduserkyc/:id',userController.adduserkyc)
+router.post('/login', userController.login)
+router.get('/getUser', userController.getUser)
+router.put('/edituser/:id', userMiddleware, userController.edituser)
+router.get('/getuserwithinvestment/:id', userController.getuserwithinvestment)
+router.get('/getuserkyc/:id', userController.getuserkyc)
+router.put('/adduserkyc/:id', userController.adduserkyc)
 module.exports = router
