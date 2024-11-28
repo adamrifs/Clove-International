71fe817a67345b28a3d47066dc70deebc5f89738
adamrifs <rifsadam81@gmail.com>
2024-11-29 00:16:12 +0530

    multer updates

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 // app.use(cors())
-app.use(cors({ origin: 'https://clove-international.vercel.app/' }));
+app.use(cors({ origin: 'https://clove-international.vercel.app' }));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
