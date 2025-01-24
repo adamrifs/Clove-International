71558efb9dc6a6bb9a59f55dd87499576037b234
adamrifs <rifsadam81@gmail.com>
2025-01-24 22:30:38 +0530

    admin auth

--- Config/utils.js
+++ Config/utils.js
@@ -7,7 +7,6 @@ const generateToken = (userId, res) => {
     res.cookie('jwt', token, {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly:true,
-        secure:process.env.NODE_ENV === 'production',
         sameSite:'strict'
     })
     return token
