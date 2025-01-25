580c4f40d4bfcee6eb7a09a02d2d6050b95dd093
adamrifs <rifsadam81@gmail.com>
2025-01-25 11:02:20 +0530

    admin auth

--- Middleware/adminMiddleware.js
+++ Middleware/adminMiddleware.js
@@ -5,13 +5,18 @@ dotenv.config()
 
 const protectRoute = async (req, res, next) => {
     try {
-        console.log('req.cookies:', req.cookies);
         console.log('req.headers:', req.headers);
+        const authHeader = req.headers.authorization;
 
-        const token = req.cookies.jwt
-        if (!token) {
-            res.status(500).json({ message: 'token required' })
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            return res.status(401).json({ message: 'Authorization header missing or invalid' });
         }
+
+        const token = authHeader.split(' ')[1];
+        // const token = req.cookies.jwt
+        // if (!token) {
+        //     res.status(500).json({ message: 'token required' })
+        // }
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         if (!decoded) {
             res.status(500).json({ message: 'unauthorized login' })
